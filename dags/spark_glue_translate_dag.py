7f0d78a43a18ca0d248aa675980fd0b9f6a4d0b1
quang-tran-vns <quang.tran@vnsilicon.net>
2026-01-14 14:05:46 +0700

    Fix volume_mounts using V1VolumeMount

--- dags/spark_glue_translate_dag.py
+++ dags/spark_glue_translate_dag.py
@@ -1,6 +1,19 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from datetime import datetime
+from kubernetes.client import models as k8s
+volume_mount = k8s.V1VolumeMount(
+    name="airflow-data",
+    mount_path="/opt/airflow/data",
+    read_only=False
+)
+
+volume = k8s.V1Volume(
+    name="airflow-data",
+    host_path=k8s.V1HostPathVolumeSource(
+        path="/opt/airflow/data"
+    )
+)
 
 with DAG(
     dag_id="spark_glue_translate_th_en",
@@ -11,29 +24,15 @@ with DAG(
 ) as dag:
 
     run_spark_etl = KubernetesPodOperator(
-        task_id="run_spark_glue_etl",
-        name="spark-glue-etl",
+        task_id="spark_glue_translate",
+        name="spark-glue-translate",
         namespace="airflow",
         image="spark-glue:1.0",
-
-        # ENTRYPOINT = spark-submit
+        cmds=["spark-submit"],
         arguments=["/app/glue_etl_translate_th_en.py"],
-
+        volumes=[volume],
+        volume_mounts=[volume_mount],
         get_logs=True,
         is_delete_operator_pod=True,
-
-        volumes=[
-            {
-                "name": "airflow-data",
-                "persistentVolumeClaim": {
-                    "claimName": "airflow-data"
-                },
-            }
-        ],
-        volume_mounts=[
-            {
-                "name": "airflow-data",
-                "mountPath": "/opt/airflow/data",
-            }
-        ],
     )
+
